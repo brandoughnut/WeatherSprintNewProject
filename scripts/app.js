274f4837c622489727dc576b4d8941fa41fe7d0e
brandoughnut <brandonnguyen2867@gmail.com>
2023-12-04 16:02:18 -0800

    5day forecast function

--- scripts/app.js
+++ scripts/app.js
@@ -16,17 +16,12 @@ async function weatherAPI() {
 }
 
 async function weather5DayAPI() {
-  const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=38.575764&lon=-121.478851&appid=${apiKey}`);
+  const promise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=38.575764&lon=-121.478851&appid${apiKey}`);
 
   const data = await promise.json();
 
 
-  console.log(data.main.temp);
-  console.log(data.main.feels_like);
-  console.log(data.main.temp_min);
-  console.log(data.main.temp_max);
-  console.log(data.main.humidity);
-  console.log(data.weather[0].main);
+  console.log(data.list[0].main.temp);
 
 }
 
